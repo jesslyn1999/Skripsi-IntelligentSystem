f18100a81b382838a11e1b6b6c40a5ac69814238
unknown <jesslynnathania99@gmail.com>
2021-06-06 12:32:45 +0700

    fix bug in writing file process yolo

--- backend_yolo.py
+++ backend_yolo.py
@@ -1,21 +1,15 @@
 import os
 import numpy as np
 
-import argparse
 import time
-from pathlib import Path
 
-import cv2
 import torch
-import torch.backends.cudnn as cudnn
 from core.utils import logging
 
 from utils.datasets import letterbox
 from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages
 from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
     scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path, save_one_box
-from utils.plots import colors, plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized
 
 from core.utils import mkdir, to_cpu
@@ -68,16 +62,18 @@ def process_frame_yolo(sys_opt: dict, dataset_loader, det_label_dir: str):
         img0 = to_cpu(key_frame).numpy()[:, :, :, ::-1]  # convert to BGR
         assert img0 is not None, 'Image Not Rendered'
 
-        img = []
-        for idx, im in enumerate(img0):
-            img.append(letterbox(im, imgsz, stride=stride)[0])
-        img = np.asarray(img)
+        imgs = []
+        for im in img0:
+            img = letterbox(im, imgsz, stride=stride)[0]
+            imgs.append(img)
+
+        img = np.asarray(imgs)
 
         # Convert
         img = img[:, :, :, ::-1].transpose(0, 3, 1, 2)  # BGR to RGB, to 3x416x416
         img = np.ascontiguousarray(img)
 
-        img = torch.from_numpy(img).to(device)
+        img = torch.from_numpy(img).to(device, non_blocking=True)
         img = img.half() if half else img.float()  # uint8 to fp16/32
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
         if img.ndimension() == 3:
@@ -96,7 +92,7 @@ def process_frame_yolo(sys_opt: dict, dataset_loader, det_label_dir: str):
             # gn = torch.tensor(img0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             if len(det):
                 # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0[i].shape).round()
 
                 # Write results
                 n_digits = len(str(n_frames[i]))
@@ -104,16 +100,16 @@ def process_frame_yolo(sys_opt: dict, dataset_loader, det_label_dir: str):
                 detection_path = os.path.join(det_label_dir, "{}.txt".format(
                     str(f_idx + 1).zfill(n_digits)))
 
-                for *xyxy, conf, cls in reversed(det):
-                    # xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                    line = (*xyxy, conf) if opt_save_conf else (cls, *xyxy)  # label format
-                    with open(detection_path, 'w+') as f:
+                with open(detection_path, 'w+') as f:
+                    for *xyxy, conf, cls in reversed(det):
+                        # xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                        line = (*xyxy, conf) if opt_save_conf else (cls, *xyxy)  # label format
                         f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
             # Print time (inference + NMS)
             # print(f'Done. ({t2 - t1:.3f}s)')
 
-        if batch_idx % 20 == 0:
+        if batch_idx % 200 == 0:
             logging(f"Progress: [%d/%d]. Done. ({t2 - t1:.3f}s)" % (batch_idx, nbatch))
 
     print(f'Done. ({time.time() - t0:.3f}s)')
--- system_main.py
+++ system_main.py
@@ -7,9 +7,11 @@ from backend_yowo import process_frame_yowo, generate_dataset_loader
 
 
 def main():
-    video_path = "D:\\semester 8\\TA\\IntelligentSystem-Result\\shootgun-video.mp4"
+    # video_path = "D:\\semester 8\\TA\\IntelligentSystem-Result\\S002C002P003R001A045_rgb.avi"
     sys_cfg_path = "D:\\semester 8\\TA\\IntelligentSystem\\config\\system\\sys_config.cfg"
-    det_label_dir = "D:\\semester 8\\TA\\IntelligentSystem-Result\\shootgun-video\\det-label"
+    video_path = "D:\\semester 8\\TA\\IntelligentSystem-Result\\S002C002P003R001A045_rgb.avi"
+    det_label_dir = "D:\\semester 8\\TA\\IntelligentSystem-Result\\trash-S002C002P003R001A045_rgb\\det-label"
+    # det_label_dir = "D:\\semester 8\\TA\\IntelligentSystem-Result\\S002C002P003R001A045_rgb-2\\det-label"
 
     sys_opt: dict = read_data_cfg(sys_cfg_path)
     opt_cfg_data = sys_opt["cfg_data"]
@@ -27,10 +29,10 @@ def main():
     test_loader = generate_dataset_loader(video_path, opt_num_workers, opt_batch_size)
 
     # process_frame_yowo(sys_opt, test_loader, yowo_label_folder)
-    # process_frame_yolo(sys_opt, test_loader, yolo_label_folder)
-    process_label_video(video_path, final_label_folder, yolo_label_folder, yowo_label_folder)
-    video_bbox(video_path, final_video_path, det_folder=final_label_folder)
-    video_bbox(video_path, final_vid_yowo_path, det_folder=yowo_label_folder)
+    process_frame_yolo(sys_opt, test_loader, yolo_label_folder)
+    # process_label_video(video_path, final_label_folder, yolo_label_folder, yowo_label_folder)
+    # video_bbox(video_path, final_video_path, det_folder=final_label_folder)
+    # video_bbox(video_path, final_vid_yowo_path, det_folder=yowo_label_folder)
 
 
 if __name__ == '__main__':
